b817e73f4d256a5cd88b0a871bab630c15c77f7e
Magnus Chr. Bareid <magnucb@astro.uio.no>
2016-10-06 16:23:56 +0200

    inital result

--- interacting.py
+++ interacting.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as pl
 import sys
 import os
+from matplotlib import rc
+rc('font',**{'family':'serif'})
 
 """
 Find Eigenvalues of Schrodinger equation for 
@@ -9,7 +11,7 @@ three values of rho_N, and several values of N.
 When are they sufficently close to known solution?
 """
 
-def get_arrays(filename, length):
+def get_arrays(filename):
     #fetch data from file
     with open(filename, 'r') as infile:
         data = infile.read()
@@ -20,17 +22,17 @@ def get_arrays(filename, length):
     eigvals = []
     eigvecs = []
     #sort out columns as arrays in dictionary
-    for line in range(len(data_splat)):
+    for line in np.range(len(data_splat)):
         if line % 2 == 0:
             eigvals.append(np.float64(data_splat[line][0]))
         else:
             eigvecs.append(pl.array(data_splat[line].split(', ')).astype(np.float64))
-    data_dict = {'lambdas': np.array(eigvals), 'eigvecs': np.array(eigvecs)}
+    data_dict = {'lambda': np.array(eigvals), 'eigvecs': np.array(eigvecs)}
 
     return data_dict
 
 def write2file(outstring,
-               filename=curdir+"/data/noninteracting.dat",
+               filename=curdir+"/data/interacting.dat",
                append=True):
     """
     If 'append' is True:
@@ -54,47 +56,60 @@ def write2file(outstring,
 
 #print get_arrays(filename="data/test.dat", length=3)
 
-eig_tol = 1e-4
-eig_known = np.array([3.0, 7.0, 11.0])
-rho_N_range = np.array([1.0, 5.0, 10.0])
-N_range = np.array([50, 100, 500, 1000])
+eig_tol     = 1e-4
+eig_known   = np.array([3.0, 7.0, 11.0])
+rho_n       = 5. # tbd
+n           = 500 # tbd
 write2file("New data-file", append=False)
 
-# ./main.cpp n rho_N
+rho0        = 0.
+omega       = np.array([0.01, 0.5, 1., 5.]).
+
+    # still need the non-interacting eigvectors, made in a previous run
+old_data = get_arrays(filename="data/project2_noninteracting_rho0=0_rhoN=%d_N=%d_omega=%d.dat" % ( rho_n, n))
+non_eigvecs = old_data['eigvecs'] # non-interacting eigvecs
+
+# ./main.cpp n rho_N rho_N omega 'interact'
 #run program "Project2_QTcreator" for these cases.
-for rho_n in rho_N_range:
-    for n in N_range:
-        #run program with rho=0,..,rho_n with n steps for a non-interacting case.
-        os.system("./build-Project2_QTcreator-Desktop_Qt_5_7_0_GCC_64bit-Release/Project2_QTcreator")
-        #fetch arrays written to "data/project2_noninteracting_rho0=0_rhoN=%d_n=%d.dat"%(rho_n,n)
-        data = get_arrays(filename="data/project2_noninteracting_rho0=0_rhoN=%d_n=%d.dat"%(rho_n,n), length=n+1)
-        #sort the lambda's in increasing order
-        eig_sorted = []
-        for i in range(len(eigenvalues)):
-            lowest = eigenvalues[0]
-            for j in range(len(eigenvalues)):
-                if eigenvalues[j] <= lowest:
-                    lowest = eigenvalues[j]
-            eig_sorted[i] = lowest
+for omega in omegas:
+    #run program with rho=0,..,rho_n with n steps for a non-interacting case.
+    os.system("./build-Project2_QTcreator-Desktop_Qt_5_7_0_GCC_64bit-Release/Project2_QTcreator %d %f %f %f %s" % (n, rho0, rho_n, omega, "interact"))
+    #fetch arrays written to "data/project2_noninteracting_rho0=0_rhoN=%d_n=%d.dat"%(rho_n,n)
+    data = get_arrays( filename="data/project2_interacting_rho0=0_rhoN=%d_N=%d_omega=%d.dat" % (rho_n, n, (omega/float(1000)) ) )
 
-        #is eigenvalues withing tolerance?
-        eigenvalues = data['lambda']
-        within_tol = []
-        
-        #test the value of the three lowest eigenvalues
-        for i in range(3):
-            if abs(eig_sorted[i] - eig_known[i]) < eig_tol:
-                within_tol.append(True)
-            else:
-                within_tol.append(False)
-
-        #store result in data-file.
-        print write2file("rho_0=%f, rho_N=%f, n=%f"%(rho_0, rho_n, n))
-        if within_tol:
-            print write2file("all three eigenvalues WITHIN tolerance")
-            for i in range(3):
-                print write2file("lambda_%d = %e"%(i,eig_sorted[i]))
+    #is eigenvalues withing tolerance?
+    eigvals = data['lambda']
+    within_tol = []
+    
+    #test the value of the three lowest eigenvalues
+    for i in np.arange(3):
+        if abs(eigvals[i] - eig_known[i]) < eig_tol:
+            within_tol.append(True)
         else:
-            print write2file("all three eigenvalues OUTSIDE tolerance")
-            for i in range(3):
-                print write2file("lambda_%d = %e"%(i,eig_sorted[i]))
+            within_tol.append(False)
+
+    #store result in data-file.
+    print write2file("rho_0=%f, rho_N=%f, n=%f"%(rho_0, rho_n, n))
+    if within_tol:
+        print write2file("all three eigenvalues WITHIN tolerance")
+        for i in np.arange(3):
+            print write2file("lambda_%d = %e"%(i,eigvals[i]))
+    else:
+        print write2file("all three eigenvalues OUTSIDE tolerance")
+        for i in np.arange(3):
+            print write2file("lambda_%d = %e"%(i,eigvals[i]))
+
+    # plots eigenvectors relevant to current omega
+    pl.figure()
+    rho = np.linspace(rho0, rho_n, n)
+    eigvecs = data['eigvecs']
+    for k in range(len(eigvecs)):
+        pl.plot(rho, non_eigvecs[k], label=r'Non-interact. eigvec. $v_%d$' % k)
+        pl.plot(rho, eigvecs[k], label=r'Interact. eigvec. $v_%d$' % k)
+
+    pl.title(r'Non-interactive- vs Interactive Eigenvectors for $\omega_r$ = %.2f' % omega)
+    pl.xlabel(r'$\rho$')
+    pl.ylabel(r'$v$')
+    pl.legend(loc='best')
+    pl.savefig('interacting_eig_plot_omega=%.2f' % omega, dpi=300)
+
--- noninteracting.py
+++ noninteracting.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as pl
 import sys
 import os
+from matplotlib import rc
+rc('font',**{'family':'serif'})
 
 """
 Find Eigenvalues of Schrodinger equation for 
@@ -20,7 +22,7 @@ def get_arrays(filename, length):
     eigvals = []
     eigvecs = []
     #sort out columns as arrays in dictionary
-    for line in range(len(data_splat)):
+    for line in np.arange(len(data_splat)):
         if line % 2 == 0:
             eigvals.append(np.float64(data_splat[line][0]))
         else:
@@ -65,25 +67,17 @@ write2file("New data-file", append=False)
 for rho_n in rho_N_range:
     for n in N_range:
         #run program with rho=0,..,rho_n with n steps for a non-interacting case.
-        os.system("./build-Project2_QTcreator-Desktop_Qt_5_7_0_GCC_64bit-Release/Project2_QTcreator")
+        os.system("./build-Project2_QTcreator-Desktop_Qt_5_7_0_GCC_64bit-Release/Project2_QTcreator %d %f" % (n, rho_n))
         #fetch arrays written to "data/project2_noninteracting_rho0=0_rhoN=%d_n=%d.dat"%(rho_n,n)
-        data = get_arrays(filename="data/project2_noninteracting_rho0=0_rhoN=%d_n=%d.dat"%(rho_n,n), length=n+1)
-        #sort the lambda's in increasing order
-        eig_sorted = []
-        for i in range(len(eigenvalues)):
-            lowest = eigenvalues[0]
-            for j in range(len(eigenvalues)):
-                if eigenvalues[j] <= lowest:
-                    lowest = eigenvalues[j]
-            eig_sorted[i] = lowest
+        data = get_arrays(filename="data/project2_noninteracting_rho0=0_rhoN=%d_N=%d.dat"%(rho_n,n), length=n+1)
 
         #is eigenvalues withing tolerance?
-        eigenvalues = data['lambda']
+        eigvals = data['lambda']
         within_tol = []
         
         #test the value of the three lowest eigenvalues
-        for i in range(3):
-            if abs(eig_sorted[i] - eig_known[i]) < eig_tol:
+        for i in np.arange(3):
+            if abs(eigvals[i] - eig_known[i]) < eig_tol:
                 within_tol.append(True)
             else:
                 within_tol.append(False)
@@ -92,9 +86,22 @@ for rho_n in rho_N_range:
         print write2file("rho_0=%f, rho_N=%f, n=%f"%(rho_0, rho_n, n))
         if within_tol:
             print write2file("all three eigenvalues WITHIN tolerance")
-            for i in range(3):
-                print write2file("lambda_%d = %e"%(i,eig_sorted[i]))
+            for i in np.arange(3):
+                print write2file("lambda_%d = %e"%(i,eigvals[i]))
         else:
             print write2file("all three eigenvalues OUTSIDE tolerance")
-            for i in range(3):
-                print write2file("lambda_%d = %e"%(i,eig_sorted[i]))
+            for i in np.arange(3):
+                print write2file("lambda_%d = %e"%(i,eigvals[i]))
+
+        # plots eigenvectors relevant to current omega
+        pl.figure()
+        rho = np.linspace(rho0, rho_n, n)
+        eigvecs = data['eigvecs']
+        for k in range(len(eigvecs)):
+            pl.plot(rho, eigvecs[k], label=r'Non-interact. eigvec. $v_%d$' % k)
+
+        pl.title(r'Non-interactive eigenvectors for $\rho_N$ = %d, $N$=%d ' % (rho_n, n))
+        pl.xlabel(r'$\rho$')
+        pl.ylabel(r'$v$')
+        pl.legend(loc='best')
+        pl.savefig('non_interacting_eigvec_plot_rhoN=%d_N=%d' % (rho_n, n), dpi=300)
