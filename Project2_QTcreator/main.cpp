3425542affe46b76696a13546eed15bb9d16f42a
oyvind <oyvind.svendsen1509@gmail.com>
2016-10-06 13:12:44 +0200

    program compiles and runs, need to debug more, specifically solve_jacobi

--- Project2_QTcreator/main.cpp
+++ Project2_QTcreator/main.cpp
@@ -12,7 +12,7 @@ void jacobi_rotation(mat& A, mat& R, int k, int l, int n);
 double find_largest(mat& A, int& max_i, int& max_j, int n);
 int kronicker(int i,int j);
 bool unit_symmetry(mat& A, int n, double tol);
-double unit_orthogonality(mat* matrix, int shape, double tol);
+bool unit_orthogonality(mat& matrix, int shape, double tol);
 
 int main(int argc, char *argv[])
 {
@@ -48,11 +48,6 @@ int main(int argc, char *argv[])
         interact = true;
     }
 
-    // declare variables
-    double h;
-    int n = 10; //size of array
-    double rho_0 = 0.0; //starting-point of length-array
-    double rho_n = 10.0; //end-point of length-array
     h = (rho_n-rho_0)/(n-1); //steplength
     vec rho = linspace<vec> (rho_0, rho_n, n); //length-array
     vec lambda = zeros<vec> (n); //eigenvalue-array
@@ -67,26 +62,107 @@ int main(int argc, char *argv[])
     //solve eigenvectors and eigenvalues using jacobi's method
     solve_jacobi(A, R, lambda, n);
 
-    lambda.print(); exit(0);
+    lambda.print();
+    A.print();
+    exit(0);
     uvec indeces_sorted = sort_index(lambda); // array of indeces of lambda when sorted
 
     filename = "../data/test.dat";
-    //remove(filename) //delete old file
+    //if (interact) {filename = "../data/noninteract_project2_.dat"} else {filename = "../data/interact_project2_.dat";
     ofstream outfile;
     outfile.open(filename, std::ofstream::out);
-    for (int i=0; i<n; i++){
+    for (int i=0; i<3; i++){
         int sort_i = indeces_sorted(i);
-        outfile << "lambda_" << i << endl;
         outfile << lambda(sort_i) << endl;
-        outfile << "vec_" << i << endl;
         for (int j=0; j<n; j++){
-            outfile << R(j,sort_i) << endl;
+            outfile << R(j,sort_i) << ", ";
         }
+        outfile << endl;
     }
     outfile.close();
 
     return 0;
 } //END: main
+
+void solve_jacobi( mat &A, mat &eigvec, vec &eigval, int n){
+    //declare local variables
+    int k, l;
+    bool sym, orth;
+    double eps_iter;
+    double eps_tol = 1.0e-8;
+    double max_iter = n*n*n;
+    int iterations = 0;
+    // Setting up the eigenvector matrix as identy-matrix
+    eigvec.eye();
+
+    //check symmetry and orthogonality
+    sym = unit_symmetry(A, n, 1e-10);
+    orth = unit_orthogonality(eigvec, n, 1e-10);
+
+    //calculate epsilon of off-diagonal elements (also k,l)
+    eps_iter = find_largest(A, k, l, n);
+
+    //perform jacobi's method until off-diagonal elements are close enough to zero.
+    while(iterations<max_iter && sym && orth && eps_iter>eps_tol){
+        //perform one jacobi rotation B=S^TAS
+        jacobi_rotation(A, eigvec, k, l, n);
+        cout << k
+             << " "
+             << l
+             << endl;
+        //advance iterations, symmetry, orthogonality, epsilon, max-indeces(k,l)
+        sym = unit_symmetry(A, n, 1e-10);
+        orth = unit_orthogonality(eigvec, n, 1e-10);
+        eps_iter = find_largest(A, k, l, n);
+        iterations++;
+    }
+    if (eps_iter<eps_tol){
+        for (int i=0; i<n; i++){
+            eigval(i) = A(i,i);
+        }
+    } else {
+        eigval.zeros();
+        cout << "Epsilon did not reach low enough levels, eigenvalues not calculated." << endl;
+    }
+    cout << "Number of iterations: " << iterations << "\n";
+} //END: solve_jacobi
+
+void jacobi_rotation(mat &A, mat &R, int k, int l, int n){
+    /* Take a matrix A and turn it to matrix B by jacobi_rotation. */
+    double tau, t, s, c, a_kk, a_ll, a_ik, a_il, r_ik, r_il;
+    if (A(k,l) == 0){
+        c = 1;
+        s = 0;
+    } else {
+        tau         = (A(l,l) - A(k,k))/(2.0*A(k,l)); // find cos(2 \theta)
+        if (tau >= 0){
+            t       = -tau + sqrt(1 + tau*tau); // lowest root of tan(\theta)
+        } else {
+            t       = -tau - sqrt(1 + tau*tau); // lowest root of tan(\theta)
+        }
+        c           = 1.0/sqrt(1+t*t); // cos(\theta)
+        s           = c*t; // sin(\theta)
+    }
+    a_kk = A(k,k); a_ll = A(l,l);
+    A(k,k)  = a_kk*c*c - 2.0*A(k,l)*c*s + a_ll*s*s; //diagonal values
+    A(l,l)  = a_ll*c*c + 2.0*A(k,l)*c*s + a_kk*s*s; //diagonal values
+    A(k,l)  = 0; //set to zero
+    A(l,k)  = 0; //set to zero
+    for (int i=0; i<n; i++){
+        if (i != k && i != l){
+            a_ik = A(i,k); a_il = A(i,l);
+            A(i,k)  = a_ik*c - a_il*s;
+            A(k,i)  = A(i,k); //symmetric value
+            A(i,l)  = a_il*c + a_ik*s;
+            A(l,i)  = A(i,l); // symmetric value
+        }
+        // compute the new eigenvectors in R-matrix
+        r_ik = R(i,k);
+        r_il = R(i,l);
+        R(i,k) = c*r_ik - s*r_il;
+        R(i,l) = c*r_il + s*r_ik;
+    }
+} //END: jacobi_rotation
 
 void singular_electron_matrix(mat& A, vec& rho, double h, int n){
     /* remake the matrix A to match a single electron with
@@ -144,75 +220,6 @@ void double_electron_matrix(mat &A, vec &rho, double omega_r, double h, int n){
     }
 } //END: double_electron_matrix
 
-void solve_jacobi( mat& A, mat& eigvec, vec& eigval, int n){
-    //declaeigvece local variables
-    int k, l;
-    bool sym, orth;
-    double eps_iter;
-    double eps_tol = 1.0e-8;
-    double max_iter = n*n*n;
-    int iterations = 0;
-    // Setting up the eigenvector matrix as identy-matrix
-    eigvec.eye();
-
-    //check symmetry and orthogonality
-    sym = unit_symmetry(A, n, 1e-10);
-    orth = unit_orthogonality(eigvec, n, 1e-10);
-
-    //calculate epsilon of off-diagonal elements (also k,l)
-    eps_iter = find_largest(A, k, l, n);
-
-    //perform jacobi's method until off-diagonal elements are close enough to zero.
-    while(iterations<max_iter && sym && orth && eps_iter>eps_tol){
-        //perform one jacobi rotation B=S^TAS
-        jacobi_rotation(A, eigvec, k, l, n);
-        //advance iterations, symmetry, orthogonality, epsilon, max-indeces(k,l)
-        sym = unit_symmetry(A, n, 1e-10);
-        orth = unit_orthogonality(eigvec, n, 1e-10);
-        eps_iter = find_largest(A, k, l, n);
-        iterations++;
-    }
-    if (eps_iter>eps_tol){
-        for (int i=0; i<n; i++){
-            eigval(i) = A(i,i);
-        }
-    }
-    cout << "Number of iterations: " << iterations << "\n";
-} //END: solve_jacobi
-
-void jacobi_rotation(mat& A, mat& R, int k, int l, int n){
-    /* Take a matrix A and turn it to matrix B by jacobi_rotation. */
-    double tau, t, s, c, a_kk, a_ll, r_ik, r_il;
-
-    tau         = (A(l,l) - A(k,k))/(2.0*A(k,l)); // find cos(2 \theta)
-    if (tau >= 0){
-        t       = -tau + sqrt(1 + tau*tau); // lowest root of tan(\theta)
-    } else {
-        t       = -tau - sqrt(1 + tau*tau); // lowest root of tan(\theta)
-    }
-    c           = 1.0/sqrt(1+t*t); // cos(\theta)
-    s           = c*t; // sin(\theta)
-
-    a_kk = A(k,k); a_ll = A(l,l);
-    A(k,k)  = a_kk*c*c- 2.0*A(k,l)*c*s + a_ll*s*s; //diagonal values
-    A(l,l)  = a_ll*c*c- 2.0*A(k,l)*c*s + a_kk*s*s; //diagonal values
-    A(k,l)  = 0; //set to zero
-    A(l,k)  = 0; //set to zero
-
-    for (int i=0; i<n; i++){
-        if (i != k && i != l){
-            A(i,k)  = A(i,k)*c - A(i,l)*s;
-            A(k,i)  = A(i,k); //symmetric value
-            A(i,l)  = A(i,l)*c + A(i,k)*s;
-            A(l,i)  = A(i,l); // symmetric value
-        }
-        // compute the new eigenvectors in R-matrix
-        r_ik = R(i,k);
-        r_il = R(i,l);
-        R(i,k) = c*r_ik - s*r_il;
-        R(i,l) = c*r_il + s*r_ik;
-    }
-} //END: jacobi_rotation
 
 double find_largest(mat& A, int& max_i, int& max_j, int n){
     /* Take matrix A and calculate the difference squared between the upper triangle
@@ -229,9 +236,9 @@ double find_largest(mat& A, int& max_i, int& max_j, int n){
 
     //loop through upper tridiagonal
     for (int i=0; i<n-1; i++){
-        for (int j=i+1; j<=n; j++){
+        for (int j=i+1; j<n; j++){
             A_val = abs(A(i,j));
-            eps += 2.0*A_val*A_val
+            eps += 2.0*A_val;
             if (A_val >= max){
                 max_i = i;
                 max_j = j;
@@ -272,7 +279,7 @@ bool unit_symmetry(mat& A, int n, double tol){
     }
 } //END: unit_symmetry
 
-bool unit_orthogonality(mat* matrix, int shape, double tol){
+bool unit_orthogonality(mat& matrix, int shape, double tol){
     /*
         Takes matrix of all eigenvectors, check dot-product of all vectors.
         Create new matrix of V_i^T*V_j that should be delta_ij.
@@ -300,83 +307,6 @@ bool unit_orthogonality(mat* matrix, int shape, double tol){
         return false;
     }
 } //END: unit_orthogonality
-
-void jacobi_rotation(mat &A, mat &R, int k, int l, int n){
-    /* Take a matrix A and turn it to matrix B by jacobi_rotation. */
-    double tau, t, s, c, a_kk, a_ll, a_ik, a_il, r_ik, r_il;
-    if (A(k,l) == 0){
-        c = 1;
-        s = 0;
-    } else {
-        tau         = (A(l,l) - A(k,k))/(2.0*A(k,l)); // find cos(2 \theta)
-        if (tau >= 0){
-            t       = -tau + sqrt(1 + tau*tau); // lowest root of tan(\theta)
-        } else {
-            t       = -tau - sqrt(1 + tau*tau); // lowest root of tan(\theta)
-        }
-        c           = 1.0/sqrt(1+t*t); // cos(\theta)
-        s           = c*t; // sin(\theta)
-    }
-    a_kk = A(k,k); a_ll = A(l,l);
-    A(k,k)  = a_kk*c*c - 2.0*A(k,l)*c*s + a_ll*s*s; //diagonal values
-    A(l,l)  = a_ll*c*c + 2.0*A(k,l)*c*s + a_kk*s*s; //diagonal values
-    A(k,l)  = 0; //set to zero
-    A(l,k)  = 0; //set to zero
-    for (int i=0; i<n; i++){
-        if (i != k && i != l){
-            a_ik = A(i,k); a_il = A(i,l);
-            A(i,k)  = a_ik*c - a_il*s;
-            A(k,i)  = A(i,k); //symmetric value
-            A(i,l)  = a_il*c + a_ik*s;
-            A(l,i)  = A(i,l); // symmetric value
-        }
-        // compute the new eigenvectors in R-matrix
-        r_ik = R(i,k);
-        r_il = R(i,l);
-        R(i,k) = c*r_ik - s*r_il;
-        R(i,l) = c*r_il + s*r_ik;
-    }
-} //END: jacobi_rotation
-
-void solve_jacobi( mat &A, mat &eigvec, vec &eigval, int n){
-    //declare local variables
-    int k, l;
-    bool sym, orth;
-    double eps_iter;
-    double eps_tol = 1.0e-8;
-    double max_iter = n*n*n;
-    int iterations = 0;
-    // Setting up the eigenvector matrix as identy-matrix
-    eigvec.eye();
-
-    //check symmetry and orthogonality
-    sym = unit_symmetry(A, n, 1e-10);
-    orth = unit_orthogonality(eigvec, n, 1e-10);
-
-    //calculate epsilon of off-diagonal elements (also k,l)
-    eps_iter = find_largest(A, k, l, n);
-
-    //perform jacobi's method until off-diagonal elements are close enough to zero.
-    while(iterations<max_iter && sym && orth && eps_iter>eps_tol){
-        //perform one jacobi rotation B=S^TAS
-        jacobi_rotation(A, eigvec, k, l, n);
-
-        //advance iterations, symmetry, orthogonality, epsilon, max-indeces(k,l)
-        sym = unit_symmetry(A, n, 1e-10);
-        orth = unit_orthogonality(eigvec, n, 1e-10);
-        eps_iter = find_largest(A, k, l, n);
-        iterations++;
-    }
-    if (eps_iter<eps_tol){
-        for (int i=0; i<n; i++){
-            eigval(i) = A(i,i);
-        }
-    } else {
-        eigval.zeros();
-        cout << "Epsilon did not reach low enough levels, eigenvalues not calculated." << endl;
-    }
-    cout << "Number of iterations: " << iterations << "\n";
-} //END: solve_jacobi
 
 int unit_known(){
     /* This function will test the imbedded jacobi solver for A=[[2,1],[1,2]].
